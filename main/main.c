959428be42f1a8b35af2c87ef8e79abe511538d0
AHMED <ahmed20atif@gmail.com>
2025-12-22 01:16:07 +0200

    COMPLETE REQUST RESIVE

--- main/main.c
+++ main/main.c
@@ -5,16 +5,52 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include "esp_err.h"
+#include "led_strip.h"
 
 #define CR 0x0D
 #define LF 0x0A
 #define wait_time_out 300 // milliseconds
 #define loop_delay 5000   // milliseconds
 
+#define RGB_LED_GPIO 48 // change if your board is different
+#define LED_STRIP_LED_NUM 1
+led_strip_handle_t led_strip;
+void led_setup(void)
+{
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = RGB_LED_GPIO,
+        .max_leds = LED_STRIP_LED_NUM,
+        .led_pixel_format = LED_PIXEL_FORMAT_GRB,
+        .led_model = LED_MODEL_WS2812,
+        .flags.invert_out = false,
+    };
+
+    led_strip_rmt_config_t rmt_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .mem_block_symbols = 64,
+        .flags.with_dma = false,
+    };
+
+    ESP_ERROR_CHECK(
+        led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+}
+
+void led_blink(void)
+{
+    // WHITE
+    led_strip_set_pixel(led_strip, 0, 255, 255, 255);
+    led_strip_refresh(led_strip);
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    // OFF
+    led_strip_clear(led_strip);
+    // vTaskDelay(pdMS_TO_TICKS(500));
+}
 void app_main(void)
 {
     const uart_port_t uart_num = UART_NUM_0;
-
+    led_setup();
     // Setup UART buffered IO with event queue
     const int uart_buffer_size = (1024 * 2);
     QueueHandle_t uart_queue;
@@ -50,7 +86,7 @@ void app_main(void)
         {
             if (strncmp((const char *)data, (const char *)IDResponse_expicted, length) == 0)
             {
-                // blink LED or indicate success
+                led_blink();
             }
         }
 
